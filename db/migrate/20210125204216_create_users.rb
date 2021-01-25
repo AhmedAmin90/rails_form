e5a8e0e61a7b780cb77b54a234bfac083b691ba1
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-26 01:37:40 +0300

    add form_tag form

--- db/migrate/20210125204216_create_users.rb
+++ db/migrate/20210125204216_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :username
       t.string :email
       t.text :password
