62c641cc516a646aeb590185a3a52f7997499249
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-27 01:57:44 +0300

    change user to users, use inline and if false ways for comment code , replace /users to users_path

--- db/migrate/20210125204216_create_users.rb
+++ db/migrate/20210125204216_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :username
       t.string :email
       t.text :password
