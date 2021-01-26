475e3e42f7b68e4986cf24880448f2ab38d17c17
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-26 23:03:03 +0300

    add linter files and fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :email, presence: true
-    validates :password, presence: true, length: {minimum:6}
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
